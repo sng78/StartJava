ee08ac7cb74d020878d446d0e513747111dcac18
Serge Gorbachev <sng78@list.ru>
2023-05-07 16:27:47 +0300

    modify hw Wolf

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,71 @@
 public class Wolf {
-    String gender;
-    String name;
-    int weight;
-    int age;
-    String color;
+    private String gender;
+    private String name;
+    private int weight;
+    private int age;
+    private String color;
 
-    void walk() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void walk() {
         System.out.println("Идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Охотится");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.weight = 50;
-        wolf.age = 3;
-        wolf.color = "серый";
-        wolf.name = "Тапок";
-        wolf.gender = "кобель";
-        System.out.println("Кличка: " + wolf.name);
-        System.out.println("Пол: " + wolf.gender);
-        System.out.println("Цвет: " + wolf.color);
-        System.out.println("Возраст: " + wolf.age);
-        System.out.println("Вес: " + wolf.weight);
+        wolf.setWeight(50);
+        wolf.setAge(3);
+        wolf.setColor("серый");
+        wolf.setName("Тапок");
+        wolf.setGender("кобель");
+        System.out.println("Кличка: " + wolf.getName());
+        System.out.println("Пол: " + wolf.getGender());
+        System.out.println("Цвет: " + wolf.getColor());
+        System.out.println("Возраст: " + wolf.getAge());
+        System.out.println("Вес: " + wolf.getWeight());
         wolf.howl();
         wolf.hunt();
         wolf.run();
